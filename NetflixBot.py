0179e18b0604f0d18390cef89560b9553cb7c290
Alan Wright <alanwright47@gmail.com>
2014-05-29 14:39:33 -0400

    Initial Commit

---
+++ NetflixBot.py
@@ -0,0 +1,204 @@
+# NetflixBot
+# Created by: Alan Wright
+# wrightlyso.com
+# github.com/alanwright
+
+import praw
+import ConfigParser
+import os
+import urllib2
+import datetime
+from NetflixRoulette import *
+from os.path import sys
+from collections import deque 
+from time import sleep
+
+NETFLIX_PREFIX = 'http://netflix.com/WiMovie/'
+API_LIMIT = 25
+SLEEP_TIME = 30
+
+def main():
+	print('NetflixBot v1.1 by u/yoalan')
+	
+	#Check for config file
+	if not os.path.exists('config.cfg'):
+		print('No config file.')
+		sys.exit()
+
+	#Fetch config settings
+	config = ConfigParser.ConfigParser()
+	config.read('config.cfg')
+	USERNAME  = config.get('Configuration', 'Username')
+	PASSWORD  = config.get('Configuration', 'Password')
+	USERAGENT = config.get('Configuration', 'Useragent')
+	SUBREDDITS = config.get('Configuration', 'Subreddit').split(',')
+	
+	if str(SUBREDDITS[0]) != 'all':
+		combined_subs = ('%s') % '+'.join(SUBREDDITS)
+		print('Looking at the following subreddits: "' + combined_subs + '".')
+	else:
+		comments = praw.helpers.comment_stream(r, 'all', limit=None)
+		print('Looking at r/all.')
+
+	#login to reddit
+	r = praw.Reddit(USERAGENT)
+	r.login(USERNAME,PASSWORD)
+
+	#Set up our cache and completed work set
+	cache = deque(maxlen=200) # double-ended queue
+	already_done = set()
+
+	running = True
+	while running:    
+		try:
+			if str(SUBREDDITS[0]) != 'all':
+				subs = r.get_subreddit(combined_subs)
+				comments = subs.get_comments()
+
+			#Check comments 
+			for c in comments:
+				
+				#Did we recently check it? If so fetch new comments
+				if c.id in cache:
+					break
+				
+				#Add this to our cache
+				cache.append(c.id)
+				
+				#Check if we need to reply
+				if check_comment(c):
+					
+					#Check if we already replied
+					for reply in c.replies:
+						if reply.author.name == USERNAME:
+							already_done.add(c.id)
+					
+					if c.id not in already_done:
+						#comment
+						movie_titles = parse_movies(c)
+						replyto(c, movie_titles, already_done)
+
+		except KeyboardInterrupt:
+			running = False
+		except Exception as e:
+			now = datetime.datetime.now()
+			print now.strftime("%m-%d-%Y %H:%M")
+			print 'ERROR:', e
+			print 'Going to sleep for 30 seconds...\n'
+			sleep(SLEEP_TIME)
+			continue
+
+#checks a comment for required text
+def check_comment(comment):
+		text = comment.body    
+		if '/u/netflixbot' in text.lower():
+			return True
+		return False
+
+#returns a list of movie titles that were in quotes in the text string
+def find_quoted_titles(text):
+	start = -1
+	end = -1
+	quotes = []
+	for i in range(0, len(text), 1):
+		if text[i] == '"' and start == -1:
+			start = i + 1
+		elif text[i] == '"' and start != -1:
+			end = i
+		if start != -1 and end != -1:
+			quotes.append(text[start:end])
+			start = -1
+			end = -1
+	return quotes
+
+#parses movie titles
+def parse_movies(comment):
+	#get the movie list
+	text = comment.body.lower()
+	begin = text.find('/u/netflixbot ') + len('/u/netflixbot ')
+	text = text[begin:]
+
+	if '"' in text:
+		splitText = text.split('"')
+		quotes = find_quoted_titles(text)
+
+		#Extract the quoted titles and split the others by ','
+		res = []
+		for item in splitText:
+
+			#needs to be split
+			if item != ' ' and item != '' and not(item in quotes):
+				tmp = item.split(',')
+				for title in tmp:
+					if title != ' ' and title != '':
+						res.append(title)
+			#quoted title
+			elif item != ' ' and item != '':
+				res.append(item)
+	else:
+		res = text.split(',')
+
+	#Remove leading and trailing spaces
+	for i in range(0, len(res), 1):
+		title = res[i]
+		if title[0] == ' ':
+			res[i] = title[1:]
+			title = res[i]
+		if title[len(title) - 1] == ' ':
+			res[i] = title[0:len(title) - 1]
+
+	return res
+
+#converts the unicode dictionary from JSON							
+def convert(input):
+    if isinstance(input, dict):
+        return {convert(key): convert(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
+#replies to given comment
+def replyto(c, movie_titles, done):
+	
+	text = ''
+	flag = False
+	movie_titles = movie_titles[0:API_LIMIT] #We can't spam API calls
+	for movie in movie_titles:
+		
+		try:
+			#Check for the last movie in a list
+			if '\n' in movie:
+				flag = True
+				movie = movie[0:movie.find('\n')]
+			
+			#Fetch data and create output string
+			data = convert(get_all_data(movie))
+			text += ('* ' + data['show_title'] + ' (' + str(data['release_year']) + ') [is available on Netflix!](' +  NETFLIX_PREFIX + str(data['show_id']) +
+					') It has a ' + str(data['rating']) + ' rating out of 5.\n')
+
+			if flag:
+				break
+
+		except urllib2.HTTPError, err:
+			# 400 error code generally means invalid query structure
+			if err.code == 400:
+				print 'Something went wrong with the query:'
+				print movie
+				print data
+			text += '* ' + movie + ' is not available on Netflix :(\n'
+
+	text += '\n\n[How to use NetflixBot.](https://github.com/alanwright/RedditBots/blob/master/bots/NetflixBot/NetflixBot_ReadMe.md)\n\n'
+	text += '*Note: Titles must match exactly.*\n\n'
+	text += "PM for Feedback | [Source Code](https://github.com/alanwright/RedditBots/blob/master/bots/NetflixBot/NetflixBot.py) | This bot uses the [NetflixRouletteAPI](http://netflixroulette.net/api/)"
+	
+	now = datetime.datetime.now()
+	print (len(done) + 1), 'ID:', c.id, 'Author:', c.author.name, 'r/' + str(c.subreddit.display_name), 'Title:', c.submission.title
+	print now.strftime("%m-%d-%Y %H:%M"), '\n'
+	c.reply(text)
+	done.add(c.id)
+	
+#call main function
+main()
---
+++ ReadMe.md
@@ -0,0 +1,67 @@
+NetflixBot
+===============================
+
+NetflixBot is designed to be a bot that listens to specific movie subreddits and when called upon (correctly) replies with whether a movie is currently streamable on Netflix (in the US).
+
+## How to Call NetflixBot
+Calling NetflixBot is simple, but if you enter an incorrect character (as of now) it may return inaccurate information, OR may not recognize a call. The proper call syntax is:
+
+For a single movie:
+
+> /u/netflixbot movie1
+
+For a list of movies:
+
+> /u/netflixbot movie1,movie2,movie3
+
+OR...
+
+> /u/netflixbot movie1, movie2, movie3
+
+If a title has comma in it, you should specify the title in quotes:
+
+> /u/netflixbot Breaking Bad, "Lock, Stock and Two Smoking Barrels", Don Jon
+
+**Important Notes**
+* It must be "/u/netflixbot" with both '/'
+* A single space between "/u/netflixbot" and the first movie title
+* The call must be on its own line OR the last thing in a comment. If you put text after the call, it needs to be on its own line
+* Capitlization does not matter
+* Title must match exactly; this causes problems for some things like "The Office" which is "The Office (U.S.)" on Netflix
+* There can be spaces between items in a list
+* You can put any amount of text before the line where you call /u/netflixbot or after (careful of bullet 3)
+
+## Current Subreddits
+Currently testing on small subreddits...
+
+* [/r/movies](http://reddit.com/r/movies)
+* [/r/movieclub](http://reddit.com/r/movieclub)
+* [/r/horror](http://reddit.com/r/horror)
+* [/r/scifi](http://reddit.com/r/scifi)
+* [/r/moviesuggestions](http://reddit.com/r/MovieSuggestions)
+* [/r/botwatch](http://reddit.com/r/botwatch)
+
+*If you would like me to add your subreddit, please reach out to me.*
+
+## How to Clone/Fork and Run NetflixBot
+NetflixBot was written to run in Python 2.7 and you will need the following packages:
+* PRAW (Reddit Python API)
+* NetflixRouletteAPI
+
+To install these, first install Python 2.7, then install PIP. This is simple so do the necessary searching. Then run the following commands:
+
+> pip install praw
+
+
+> pip install NetflixRouletteAPI
+
+You will also need to edit the config.cfg file with your bots credentials. The code should now compile and run :)
+
+##Additional Notes
+*Why is it US only? Reddit is an international community!*
+
+Right now the NetflixRouletteAPI only supports the US, and since I use this API, I can currently only support US accuracy. If this bot becomes popular I would like to increase its scope. 
+
+Alan Wright
+
+[WrightlySo](www.wrightlyso.com)
